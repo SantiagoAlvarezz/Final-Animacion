28d93cd18bf2f3fe93e0c07c01f963111b7265b0
SantiagoAlvarezz <santiagoalvarez2283@gmail.com>
2023-05-19 14:24:21 -0500

    Add Buttons
    Next Scene and Prev Scene

---
+++ Final-anim/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadPreviousScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
